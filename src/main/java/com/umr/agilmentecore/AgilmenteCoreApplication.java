a054667fe439838adf8107dc4ae79bcfc257f599
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2021-05-26 19:54:21 -0300

    Feat: Created Spring Boot project

---
+++ src/main/java/com/umr/agilmentecore/AgilmenteCoreApplication.java
@@ -0,0 +1,13 @@
+package com.umr.agilmentecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilmenteCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilmenteCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umr/agilmentecore/AgilmenteCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umr.agilmentecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilmenteCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
