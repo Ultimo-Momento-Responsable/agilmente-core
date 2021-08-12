157719238bd295b9c600e630b8f48b9c9d64fcf3
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2021-08-12 17:03:37 -0300

    :sparkles: Feat (planning): add endpoint planning by patient id

--- src/main/java/com/umr/agilmentecore/Controller/PlanningController.java
+++ src/main/java/com/umr/agilmentecore/Controller/PlanningController.java
@@ -1,10 +1,13 @@
 package com.umr.agilmentecore.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +42,14 @@ public class PlanningController {
 	@PostMapping
 	public Planning save(@RequestBody PlanningData planning) throws Exception {
 		return this.service.save(planning);
+	}
+	/**
+	 * Obtiene una Planificación.
+	 * @param Long el id del paciente específico.
+	 * @return Optional una planificación o nada.
+	 */
+	@GetMapping(value = "/patient_{id}")
+	public List<Planning> getCurrentPlanningsFromPatient(@PathVariable(name = "id") Long id) {
+		return service.getCurrentPlanningsFromPatient(id);
 	}
 }
--- src/main/java/com/umr/agilmentecore/Services/PlanningService.java
+++ src/main/java/com/umr/agilmentecore/Services/PlanningService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
