565152d3667f2ca0a394885bce2a5ed0b4a95860
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2021-09-09 18:25:15 -0300

    :wrench: Chore (Result): remove unused import

--- src/main/java/com/umr/agilmentecore/Controller/GameSessionResultController.java
+++ src/main/java/com/umr/agilmentecore/Controller/GameSessionResultController.java
@@ -2,7 +2,6 @@ package com.umr.agilmentecore.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
