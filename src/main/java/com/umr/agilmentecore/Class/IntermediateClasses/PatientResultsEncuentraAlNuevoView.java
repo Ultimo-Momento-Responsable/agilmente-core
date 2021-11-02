e5979b5fb06558fde2b4fa2b36a6856e3c0b4a96
rassyu <andresmatter@hotmail.com>
2021-11-02 19:08:57 -0300

    :bug: Fix (fixed-results-typo): fixed typo in getResults

--- src/main/java/com/umr/agilmentecore/Class/IntermediateClasses/PatientResultsEncuentraAlNuevoView.java
+++ src/main/java/com/umr/agilmentecore/Class/IntermediateClasses/PatientResultsEncuentraAlNuevoView.java
@@ -1,5 +1,22 @@
 package com.umr.agilmentecore.Class.IntermediateClasses;
 
+import java.util.List;
+
+import com.umr.agilmentecore.Class.EncuentraAlNuevoResult;
+
 public class PatientResultsEncuentraAlNuevoView {
-	public PatientResultsEncuentraAlNuevoView() {}
+	private List<EncuentraAlNuevoResult> results;
+	
+	public PatientResultsEncuentraAlNuevoView(List<EncuentraAlNuevoResult> results) {
+		super();
+		this.results = results;
+	}
+	
+	public List<EncuentraAlNuevoResult> getResults() {
+		return results;
+	}
+	
+	public void setResults(List<EncuentraAlNuevoResult> results) {
+		this.results = results;
+	}
 }
