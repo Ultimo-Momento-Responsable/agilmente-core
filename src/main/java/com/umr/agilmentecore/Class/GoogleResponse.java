5c6e65d31382e1b0cce2e02c47a6877cde8cd961
rassyu <andresmatter@hotmail.com>
2022-10-12 16:44:10 -0300

    :art: Style (classes): added code linting to various classes

--- src/main/java/com/umr/agilmentecore/Class/GoogleResponse.java
+++ src/main/java/com/umr/agilmentecore/Class/GoogleResponse.java
@@ -13,29 +13,22 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonPropertyOrder({
-    "success",
-    "score", 
-    "action",
-    "challenge_ts",
-    "hostname",
-    "error-codes"
-})
+@JsonPropertyOrder({ "success", "score", "action", "challenge_ts", "hostname", "error-codes" })
 public class GoogleResponse {
 	@JsonProperty("success")
-    private boolean success;
+	private boolean success;
 	@JsonProperty("challenge_ts")
-    private String challengeTs;
-    @JsonProperty("hostname")
-    private String hostname;
-    @JsonProperty("score")
-    private float score;
-    @JsonProperty("action")
-    private String action;
-    @JsonProperty("error-codes")
-    private ErrorCode[] errorCodes;
-    
-    public boolean isSuccess() {
+	private String challengeTs;
+	@JsonProperty("hostname")
+	private String hostname;
+	@JsonProperty("score")
+	private float score;
+	@JsonProperty("action")
+	private String action;
+	@JsonProperty("error-codes")
+	private ErrorCode[] errorCodes;
+
+	public boolean isSuccess() {
 		return success;
 	}
 
@@ -82,49 +75,50 @@ public class GoogleResponse {
 	public void setErrorCodes(ErrorCode[] errorCodes) {
 		this.errorCodes = errorCodes;
 	}
-    
-    enum ErrorCode {
-        MissingSecret, InvalidSecret, MissingResponse, InvalidResponse, BadRequest, TimeoutOrDuplicate;
 
-        private static Map<String, ErrorCode> errorsMap = new HashMap<>(6);
+	enum ErrorCode {
+		MissingSecret, InvalidSecret, MissingResponse, InvalidResponse, BadRequest, TimeoutOrDuplicate;
 
-        static {
-            errorsMap.put("missing-input-secret", MissingSecret);
-            errorsMap.put("invalid-input-secret", InvalidSecret);
-            errorsMap.put("missing-input-response", MissingResponse);
-            errorsMap.put("bad-request", InvalidResponse);
-            errorsMap.put("invalid-input-response", BadRequest);
-            errorsMap.put("timeout-or-duplicate", TimeoutOrDuplicate);
-        }
+		private static Map<String, ErrorCode> errorsMap = new HashMap<>(6);
 
-        @JsonCreator
-        public static ErrorCode forValue(final String value) {
-            return errorsMap.get(value.toLowerCase());
-        }
-    }
-    
-    @JsonIgnore
-    public boolean hasClientError() {
-        final ErrorCode[] errors = getErrorCodes();
-        if (errors == null) {
-            return false;
-        }
-        for (final ErrorCode error : errors) {
-            switch (error) {
-            case InvalidResponse:
-            case MissingResponse:
-            case BadRequest:
-                return true;
-            default:
-                break;
-            }
-        }
-        return false;
-    }
+		static {
+			errorsMap.put("missing-input-secret", MissingSecret);
+			errorsMap.put("invalid-input-secret", InvalidSecret);
+			errorsMap.put("missing-input-response", MissingResponse);
+			errorsMap.put("bad-request", InvalidResponse);
+			errorsMap.put("invalid-input-response", BadRequest);
+			errorsMap.put("timeout-or-duplicate", TimeoutOrDuplicate);
+		}
 
-    @Override
-    public String toString() {
-        return "GoogleResponse{" + "success=" + success + ", challengeTs='" + challengeTs + '\'' + ", hostname='" + hostname + '\''+ ", score='" + score + '\''+ ", action='" + action+ '\'' + ", errorCodes=" + Arrays.toString(errorCodes) + '}';
-    }
+		@JsonCreator
+		public static ErrorCode forValue(final String value) {
+			return errorsMap.get(value.toLowerCase());
+		}
+	}
+
+	@JsonIgnore
+	public boolean hasClientError() {
+		final ErrorCode[] errors = getErrorCodes();
+		if (errors == null) {
+			return false;
+		}
+		for (final ErrorCode error : errors) {
+			switch (error) {
+			case InvalidResponse:
+			case MissingResponse:
+			case BadRequest:
+				return true;
+			default:
+				break;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "GoogleResponse{" + "success=" + success + ", challengeTs='" + challengeTs + '\'' + ", hostname='"
+				+ hostname + '\'' + ", score='" + score + '\'' + ", action='" + action + '\'' + ", errorCodes="
+				+ Arrays.toString(errorCodes) + '}';
+	}
 }
-    
--- src/main/java/com/umr/agilmentecore/Services/CaptchaService.java
+++ src/main/java/com/umr/agilmentecore/Services/CaptchaService.java
@@ -17,32 +17,32 @@ import com.umr.agilmentecore.Class.GoogleResponse;
 public class CaptchaService {
 
 	@Autowired
-    private RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 	public static final String REGISTER_ACTION = "register";
 
 	@Value("${google.recaptcha.key.threshold}")
 	private float threshold;
-	
+
 	@Value("${google.recaptcha.key.secret}")
 	private String secret;
-	
+
 	@Autowired
-    protected HttpServletRequest request;
-	
+	protected HttpServletRequest request;
+
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-	    return builder.build();
+		return builder.build();
 	}
-	
+
 	protected static final String RECAPTCHA_URL_TEMPLATE = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
-	
-    public boolean processResponse(String response, String action) {
-    	final URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, secret, response));
-        GoogleResponse googleResponse = restTemplate.getForObject(verifyUri, GoogleResponse.class);        
-        if(googleResponse.getScore() < threshold) {
-            return false;
-        }
-        return true;
-    }
+
+	public boolean processResponse(String response, String action) {
+		final URI verifyUri = URI.create(String.format(RECAPTCHA_URL_TEMPLATE, secret, response));
+		GoogleResponse googleResponse = restTemplate.getForObject(verifyUri, GoogleResponse.class);
+		if (googleResponse.getScore() < threshold) {
+			return false;
+		}
+		return true;
+	}
 
 }
