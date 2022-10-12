3709b4ab7ca390d6e0822c9a5431132f57568a81
Fenlethmarg <guillermo.gribaudo95@gmail.com>
2022-10-12 19:18:03 -0300

    Update LoginController.java

--- src/main/java/com/umr/agilmentecore/Controller/LoginController.java
+++ src/main/java/com/umr/agilmentecore/Controller/LoginController.java
@@ -15,46 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.umr.agilmentecore.Class.IntermediateClasses.ChangePassword;
 import com.umr.agilmentecore.Class.IntermediateClasses.LoginData;
 import com.umr.agilmentecore.Class.IntermediateClasses.ProfessionalData;
-import com.umr.agilmentecore.Services.CaptchaService;
 import com.umr.agilmentecore.Services.LoginService;
 
 import io.jsonwebtoken.security.InvalidKeyException;
+
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-
+	
 	@Autowired
 	private LoginService service;
-	@Autowired
-	private CaptchaService captchaService;
-
+	
 	/**
-	 * Función que se encarga de chequear el login y setear el tiempo de expiración
-	 * del mismo.
-	 * 
+	 * Función que se encarga de chequear el login y setear el tiempo de expiración del mismo.
 	 * @param user Objeto con usuario y contraseña
 	 * @return Devuelve el token de inicio de sesión o nulo si el usuario no existe
-	 * @throws UnsupportedEncodingException
-	 * @throws NoSuchAlgorithmException
-	 * @throws InvalidKeyException
+	 * @throws UnsupportedEncodingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
 	 */
-	@PostMapping
-	public ProfessionalData login(@RequestBody LoginData user)
-			throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
-		return service.login(user);
-	}
-
-	/**
-	 * Endpoint que se encarga de cambiar la contraseña del profesional
-	 * @param user contiene una contraseña vieja, una nueva y el id del profesional
-	 * @return true o false si se puede cambiar o no.
-	 */
-	@PostMapping(value = "/changePassword")
-	public Boolean changePassword(@RequestBody ChangePassword user) {
-		return service.changePassword(user);
-	}
+	@PostMapping	
+    public ProfessionalData login(@RequestBody LoginData user) throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+        return service.login(user);
+    }
 	
 	/**
 	 * Endpoint que se encarga de cambiar la contraseña del profesional
@@ -68,17 +53,11 @@ public class LoginController {
 	
 	/**
 	 * Chequea si el token es válido y si no se ha expirado
-	 * 
 	 * @param token el token que viene del front
 	 * @return devuelve true o false si es válido o no el token
 	 */
-	@GetMapping(value = "/token/{token}")
-	public Boolean checkIfLogged(@PathVariable(name = "token") String token) {
-		return service.checkIfLogged(token);
-	}
-
-	@PostMapping("/loginCaptcha")
-	public Boolean captchaLoginAccount(@RequestBody String token) {
-		return captchaService.processResponse(token, CaptchaService.REGISTER_ACTION);
-	}
+	@GetMapping(value = "/token/{token}")	
+    public Boolean checkIfLogged(@PathVariable(name = "token") String token) {
+        return service.checkIfLogged(token);
+    }
 }
